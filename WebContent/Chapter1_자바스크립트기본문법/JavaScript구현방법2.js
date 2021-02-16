c72a8dd6f21556274ea090bc42355daff7e74307
xorrb <xorrb@DESKTOP-QKUE1JR>
2021-02-16 18:06:30 +0900

    Script

---
+++ WebContent/Chapter1_자바스크립트기본문법/JavaScript구현방법2.js
@@ -0,0 +1,8 @@
+function over(obj){
+		obj.src="../img/뚱이.png";
+	
+}
+function out(obj){
+		obj.src="../img/슈렉.png"
+	
+}
---
+++ WebContent/Chapter1_자바스크립트기본문법/welcome.js
@@ -0,0 +1,8 @@
+	var subject1 = "Welcome to JavaScript<br>";	/* var=변수선언 */
+	var subject2 = "Enjoy JavaScript Programming!";
+	function welcome(target) {
+		document.write(target);
+	}
+	
+	welcome(subject1);
+	welcome(subject2);
